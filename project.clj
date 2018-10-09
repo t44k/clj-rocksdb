1c3df67aee6cbc28553e085349a2db00fa00e910
Tamas Korodi <tkorodi@me.com>
2018-10-09 16:31:57 +0200

    lein-release plugin: bumped version from 0.1.2 to 0.1.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kotyo/clj-rocksdb "0.1.2"
+(defproject kotyo/clj-rocksdb "0.1.3-SNAPSHOT"
   :description "Clojure bindings for facebook's rocksdbjni"
   :url "http://github.com/kotyo/clj-rocksdb"
   :license {:name "Eclipse Public License"
