eb2ed09d3b96fa67330157d2c6df5c4ccadabaa4
Tamas Korodi <tkorodi@me.com>
2018-10-15 10:45:58 +0200

    Upgrade to rocksdbjni version: 5.15.10

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/kotyo/clj-rocksdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.rocksdb/rocksdbjni "5.14.2"]
+  :dependencies [[org.rocksdb/rocksdbjni "5.15.10"]
                  [byte-streams "0.2.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [com.taoensso/nippy "2.14.0"]]}})
