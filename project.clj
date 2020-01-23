9030a1e4c493d487762d6507934227e63914c75d
Tamas Korodi <tkorodi@me.com>
2020-01-23 20:54:38 +0100

    Update rocksdbjni version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/kotyo/clj-rocksdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.rocksdb/rocksdbjni "5.15.10"]
+  :dependencies [[org.rocksdb/rocksdbjni "6.4.6"]
                  [byte-streams "0.2.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [com.taoensso/nippy "2.14.0"]]}})
