2f437e800bb3a7cbf4b01b296cf840548c47e774
Tamas Korodi <tkorodi@me.com>
2020-05-18 21:14:32 +0200

    lein-release plugin: preparing 0.1.6 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kotyo/clj-rocksdb "0.1.6-SNAPSHOT"
+(defproject kotyo/clj-rocksdb "0.1.6"
   :description "Clojure bindings for facebook's rocksdbjni"
   :url "http://github.com/kotyo/clj-rocksdb"
   :license {:name "Eclipse Public License"
